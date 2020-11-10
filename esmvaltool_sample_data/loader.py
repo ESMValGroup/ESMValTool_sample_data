e46ae61f1f8e2d893cf1f50b1ef15a9339aa98fc
Stef Smeets <s.smeets@esciencecenter.nl>
2020-11-10 11:44:43 +0100

    Address review comments

--- esmvaltool_sample_data/loader.py
+++ esmvaltool_sample_data/loader.py
@@ -5,6 +5,14 @@ import iris
 
 base_dir = Path(__file__).parent
 
+problematic = [
+    # iris.exceptions.ConcatenateError: failed to concatenate into a single cube.
+    'esmvaltool_sample_data/data/timeseries/CMIP6/CMIP/NCC/NorCPM1/historical/r1i1p1f1/Amon/ta/gn/v20190914',
+    # UserWarning: Gracefully filling 'lat' dimension coordinate masked points
+    'esmvaltool_sample_data/data/timeseries/CMIP6/CMIP/NCAR/CESM2-FV2/historical/r1i1p1f1/Amon/ta/gn/v20191120',
+    'esmvaltool_sample_data/data/timeseries/CMIP6/CMIP/NCAR/CESM2-WACCM-FV2/historical/r1i1p1f1/Amon/ta/gn/v20191120',
+]
+
 
 def strip_attributes(cube):
     """Remove attributes that cause issues with merging and concatenation."""
@@ -23,21 +31,24 @@ def simplify_time(cube):
 def load_cubes_from_input_dirs(input_dirs):
     """Loads all *.nc files from each input dir into a cube."""
     for input_dir in input_dirs:
+        if str(input_dir) in problematic:
+            # print('Skipping', input_dir)
+            continue
+        # print(input_dir)
         files = input_dir.glob('*.nc')
         cubes = iris.load(str(file) for file in files)
         for cube in cubes:
             strip_attributes(cube)
             simplify_time(cube)
 
-        cubes = cubes.concatenate()
-        cube = cubes[0]
+        cube = cubes.concatenate_cube()
 
         yield cube
 
 
-def load_timeseries_cubes():
+def load_timeseries_cubes(mip_table='Amon'):
     """
-    Data: ta / Amon / historical / r1i1p1f1, any grid, 1850 - onwards.
+    Data: ta / Amon / historical / r1i1p1f1, any grid, 1950 - onwards.
     All dimensions reduced to a few steps except for the time dimension
     Some other variable / ocean, probably a different frequency,
        similar number of timesteps, other dimensions reduced.
@@ -45,27 +56,8 @@ def load_timeseries_cubes():
 
     timeseries_dir = base_dir / 'data' / 'timeseries'
 
-    data_dirs = [
-        'CMIP6.CMIP.CAMS.CAMS-CSM1-0.historical.r1i1p1f1.Amon.ta.gn.v20190708',
-        'CMIP6.CMIP.CCCR-IITM.IITM-ESM.historical.r1i1p1f1.Amon.ta.gn.v20191226',
-        'CMIP6.CMIP.CSIRO-ARCCSS.ACCESS-CM2.historical.r1i1p1f1.Amon.ta.gn.v20191108',
-        'CMIP6.CMIP.E3SM-Project.E3SM-1-1.historical.r1i1p1f1.Amon.ta.gr.v20191211',
-        'CMIP6.CMIP.HAMMOZ-Consortium.MPI-ESM-1-2-HAM.historical.r1i1p1f1.Amon.ta.gn.v20190627',
-        'CMIP6.CMIP.INM.INM-CM4-8.historical.r1i1p1f1.Amon.ta.gr1.v20190605',
-        'CMIP6.CMIP.INM.INM-CM5-0.historical.r1i1p1f1.Amon.ta.gr1.v20190610',
-        'CMIP6.CMIP.IPSL.IPSL-CM6A-LR.historical.r1i1p1f1.Amon.ta.gr.v20180803',
-        'CMIP6.CMIP.MPI-M.MPI-ESM1-2-HR.historical.r1i1p1f1.Amon.ta.gn.v20190710',
-        'CMIP6.CMIP.MPI-M.MPI-ESM1-2-LR.historical.r1i1p1f1.Amon.ta.gn.v20190710',
-        'CMIP6.CMIP.NOAA-GFDL.GFDL-CM4.historical.r1i1p1f1.Amon.ta.gr1.v20180701',
-        'CMIP6.CMIP.NOAA-GFDL.GFDL-ESM4.historical.r1i1p1f1.Amon.ta.gr1.v20190726',
-
-        # BUG: next dataset is problematic
-        # raises ValueError: Cube 'air_temperature' must contain
-        #     a single 1D y coordinate.
-        # 'CMIP6.CMIP.FIO-QLNM.FIO-ESM-2-0.historical.r1i1p1f1.Amon.ta.gn.v20191204',
-    ]
-
-    input_dirs = [timeseries_dir / data_dir for data_dir in data_dirs]
+    paths = timeseries_dir.glob(f'**/{mip_table}/**/*.nc')
+    input_dirs = list(set(path.parent for path in paths))
 
     cubes = load_cubes_from_input_dirs(input_dirs)
 
