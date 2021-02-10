cc0aaf8c342f6d496b7ad612373f73ce3f82f735
Bouwe Andela <b.andela@esciencecenter.nl>
2021-02-10 12:08:41 +0100

    Update the version number and add note about PyPI installation

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ PACKAGES = [
 
 setup(
     name='ESMValTool_sample_data',
-    version='0.0.3',
+    version='0.0.4',
     description="ESMValTool sample data",
     long_description=readme,
     long_description_content_type='text/markdown',
